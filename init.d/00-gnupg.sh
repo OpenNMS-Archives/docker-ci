29272148326fd12b728f6793ba79547f961fde2e
Benjamin Reed <ranger@opennms.org>
2015-08-11 17:22:28 -0400

    spit out warnings if gnupg or m2 are not there

--- init.d/00-gnupg.sh
+++ init.d/00-gnupg.sh
@@ -4,4 +4,6 @@ if [ -d /data/gnupg ]; then
 	rsync -ar /data/gnupg/ ~/.gnupg/
 	chmod 700 ~/.gnupg/
 	chmod go-rwx ~/.gnupg/*
+else
+	echo "WARNING: no /data/gnupg directory found; you will not be able to sign packages"
 fi
--- init.d/00-m2-home.sh
+++ init.d/00-m2-home.sh
@@ -2,4 +2,6 @@
 
 if [ -d /data/m2 ]; then
 	rsync -avr /data/m2/ ~/.m2/
+else
+	echo "WARNING: no /data/m2 directory found; you will not be able to sign jars"
 fi

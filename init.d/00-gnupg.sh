b0e47e835121e4ba3b46866d559d253d8c61339f
Benjamin Reed <ranger@opennms.org>
2015-08-12 16:01:39 -0400

    don't use rsync here

--- init.d/00-gnupg.sh
+++ init.d/00-gnupg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 if [ -d /data/gnupg ]; then
-	rsync -ar /data/gnupg/ ~/.gnupg/
+	cp -pR /data/gnupg/ ~/.gnupg/
 	chmod 700 ~/.gnupg/
 	chmod go-rwx ~/.gnupg/*
 else
--- init.d/00-m2-home.sh
+++ init.d/00-m2-home.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 if [ -d /data/m2 ]; then
-	rsync -avr /data/m2/ ~/.m2/
+	cp -pR /data/m2/ ~/.m2/
 else
 	echo "WARNING: no /data/m2 directory found; you will not be able to sign jars"
 fi

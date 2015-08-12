22937d092c754ce7427af64476fb8d54f32c7d1f
Benjamin Reed <ranger@opennms.org>
2015-08-12 16:46:52 -0400

    don't rebuild if nothing has changed

--- docker-build.sh
+++ docker-build.sh
@@ -6,6 +6,14 @@ for IMAGE in \
 	opennms-compile-debhost \
 	opennms-installer-debhost \
 ; do
+	echo "=== ${IMAGE} ==="
 	rsync -ar --delete init.d/ "$IMAGE"/init.d/
+	if [ -e "${IMAGE}.built" ]; then
+		NEWFILES=`find "${IMAGE}" -type f -newer "${IMAGE}.built" | wc -l`
+		if [ $NEWFILES -eq 0 ]; then
+			continue
+		fi
+	fi
 	docker build -t "$IMAGE" "$IMAGE"
+	touch "${IMAGE}.built"
 done

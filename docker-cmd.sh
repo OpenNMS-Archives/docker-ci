caaaed35fba220a8d6d265a8d4f6bbba65be730f
Benjamin Reed <ranger@opennms.org>
2017-07-27 12:27:03 -0400

    put this back

--- docker-cmd.sh
+++ docker-cmd.sh
@@ -117,14 +117,14 @@ find . -type f -name opennms-datasources.xml | grep /src/test/ | while read -r F
 done
 
 echo "* removing failing tests for now..."
-while read -r FILE; do
+cat /blacklist-files.txt | while read -r FILE; do
 	if [ -n "$FILE" ] && [ -r "$FILE" ]; then
 		echo "* blacklisted: $FILE"
 		rm -f "$FILE"
 	else
 		echo "* not found: $FILE"
 	fi
-done < /blacklist-files.txt
+done
 
 echo "* building in $WORKDIR:"
 

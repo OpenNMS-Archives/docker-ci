ae38736fe123687edd5d9489e5abef285da648ca
Benjamin Reed <ranger@opennms.org>
2017-08-03 14:38:24 -0400

    update script

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -7,4 +7,13 @@ for container in opennms-integration-tests opennms-postgres; do
 	docker rm $container || :
 done
 docker run --name opennms-postgres -e POSTGRES_PASSWORD=stests -d postgres
-docker run --name opennms-integration-tests --link opennms-postgres:postgres opennms/itests "$@"
+docker run --name opennms-integration-tests \
+	--link opennms-postgres:postgres \
+	--sysctl 'net.ipv4.ping_group_range=0 429496729' \
+	--sysctl 'net.core.netdev_max_backlog=5000' \
+	--sysctl 'net.core.rmem_default=8388608' \
+	--sysctl 'net.core.rmem_max=16777216' \
+	--sysctl 'net.core.wmem_default=8388608' \
+	--sysctl 'net.core.wmem_max=16777216' \
+	opennmsbamboo/itests \
+	"$@"
